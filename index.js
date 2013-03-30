ca60a74e3efe1ea69f52a06d485d420d4dc848d2
Daniel Fagnan <dnfagnan@gmail.com>
2013-03-30 00:33:05 -0600

    working express server
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-function Application() {
+var container = require('tower-container')
+  , bundle = require('tower-bundle');
 
+var app = {};
+
+function create(appArg) {
+  app = appArg;
+
+  return Application;
 }
 
+function Application(name) {
+  if (!(this instanceof Application)) {
+    return new Application(name);
+  }
 
-module.exports = Application;
+  this.options = {};
+  this.app = app;
+  this.set('port', 3000);
+  this.set('name', name);
+  this.container = container;
+  this.view = {};
+  this.bundle = bundle;
+  this.model = {};
+  this.stream = {};
+  this.route = {};
+  this.server = {};
+}
+
+Application.prototype.set = function(key, value) {
+  this.app.set(key,value);
+};
+
+Application.prototype.get = function(key) {
+  return this.app.get(key);
+};
+
+Application.prototype.listen = function() {
+  this.bundle.initialize();
+  // Check if we're running in dev mode.
+  // Run the bundler:
+  //this.bundle.watch();
+
+  return this.app.listen.apply(this.app, arguments);
+}
+
+module.exports = create;

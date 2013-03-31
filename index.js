3238b97cae4e35664ecacc4352172839541b482b
Daniel Fagnan <dnfagnan@gmail.com>
2013-03-30 20:06:11 -0600

    changes
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var container = require('tower-container')
   , bundle = require('tower-bundle')
-  , ansi   = require('ansi');
+  , ansi   = require('ansi')
+  , cursor = ansi(process.stdout, { enabled: true })
+  , consoleFn = require('./lib/console');
 
 var app = {};
+var server = {};
 
-function create(appArg) {
+function create(appArg, server) {
   app = appArg;
+  server = server;
 
   return Application;
 }
@@ -15,7 +19,10 @@ function Application(name) {
     return new Application(name);
   }
 
-  this.options = {};
+  this.options = {
+    environment: server.environemnt
+  };
+
   this.app = app;
   this.set('port', 3000);
   this.set('name', name);
@@ -37,16 +44,15 @@ Application.prototype.get = function(key) {
 };
 
 Application.prototype.listen = function() {
-  this.bundle.initialize();
   // Check if we're running in dev mode.
   // Run the bundler:
   //this.bundle.watch();
-
+  this.log("server", "Tower is listening on port -> {{port}}", {
+    port: app.get('port')
+  });
   return this.app.listen.apply(this.app, arguments);
 }
 
-Application.prototype.log = function(action, message) {
-
-}
+Application.prototype.log = consoleFn;
 
 module.exports = create;

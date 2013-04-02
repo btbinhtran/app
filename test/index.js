423af3793300981a6a3c657365a93741dad987b9
Lance Pollard <lancejpollard@gmail.com>
2013-04-02 01:19:38 -0700

    fast testing for routes

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
 var app = require('..')()()
   , model = require('tower-model')
+  , route = require('tower-route')
+  , router = require('tower-router')
+  , Context = require('tower-context')
   , assert = require('assert');
 
 describe('app', function(){
@@ -8,5 +11,20 @@ describe('app', function(){
       .attr('email');
 
     assert(model === app.model);
+  });
+
+  it('should handle route', function(done){
+    var calls = 0;
+
+    route('/users', 'users.index')
+      .use(function(context, next){
+        calls++;
+        next();
+      });
+
+    router(new Context('/users'), function(){
+      assert(1 == calls);
+      done();
+    })
   });
 });
